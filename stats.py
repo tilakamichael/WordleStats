b822150c1ab4be608a8bd4d212f8db96c6ecbffa
tilakamichael <mdt53229@uga.edu>
2022-03-08 00:36:35 -0500

    Added mean calculator

--- stats.py
+++ stats.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plot
 
 def createMap():
     list = scraper.scrapeQuery("Wordle 262", 100)
+    average = 0
+    length = 0
+    
     map = { "1" : 0,
             "2" : 0,
             "3" : 0,
@@ -16,11 +19,14 @@ def createMap():
             location = i[i.index("/6") - 1]
             if(location in map):
                 map[location] = map.get(location, 0) + 1
+            
+            if (i[i.index("/6") - 1] is not "X"):
+                average += int(i[i.index("/6") - 1])                
+                length = length + 1
     
-    return map
-
+    return map, round((average / float(length)), 1)
     
-map = createMap()
+map, average = createMap()
 
 keys = map.keys()
 values = map.values()
@@ -28,4 +34,6 @@ values = map.values()
 plot.bar(keys, values)
 plot.ylabel("Number of users")
 plot.title("Wordle guesses Wordle #262")
+
+print(average)
 plot.show()
