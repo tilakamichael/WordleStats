a571e69ba8608fb2bc02e01c46f818326079368c
tilakamichael <mdt53229@uga.edu>
2022-03-16 18:32:03 -0400

    Checks to make sure num/6 is valid

--- stats.py
+++ stats.py
@@ -31,16 +31,19 @@ def createMap():
             if (ord(i[i.index("/6") - 1]) >= 49 and ord(i[i.index("/6") - 1]) <= 54):
                 mean += int(i[i.index("/6") - 1])                
                 numlength = numlength + 1
+            elif ((i[i.index("/6") - 1]).upper() == 'X'):
+                mean += 7
+                numlength = numlength + 1
     
     #Return the dictionary and the mean rounded to one decimal place
-    return map, round((mean / float(numlength)), 1), len(list)
+    return map, round((mean / float(numlength)), 1), numlength
 
 def todayWordle():
     #Set up today and start date
     today = datetime.today().strftime("%Y-%m-%d")
     startDate = "2022-03-08"
     
-    #Bases what the wordle number is on the difference between current date,  and march 8th (262, when I started)
+    #Bases what the wordle number is on the difference between current date, and march 8th (262, when I started)
     difference = datetime.strptime(today, "%Y-%m-%d") - datetime.strptime(startDate, "%Y-%m-%d")
     
     return "Wordle " + str(262 + abs(difference.days))
@@ -48,7 +51,8 @@ def todayWordle():
 
 def main():
     map, mean, length = createMap()
-
+    
+    #Gets total number of results
     print("Mean value: " + str(mean))   
 
     #Rounds to nearest integer
